d9ea679e843cbda2ad133d67a20623c3e56b1e40
amani51 <amani.898919@gmail.com>
2022-12-02 17:07:14 +0200

    edit: edit path

--- snacks/urls.py
+++ snacks/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import SnackListView,SnackDetail
 urlpatterns = [
     path('',SnackListView.as_view(),name='snacks'),
-    path('<pk>',SnackDetail.as_view(),name='detail')
+    path('<int:pk>',SnackDetail.as_view(),name='detail')
 ]
